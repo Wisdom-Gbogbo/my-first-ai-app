49e49ba3be9d4e2d08e6a841f8f0d95be9c05910
Wisdom-Gbogbo <wisdhom01@gmail.com>
2026-07-25 22:00:09 +0000

    Edited hello.py and Added Notebook to week 1 file

--- week1/hello.py
+++ week1/hello.py
@@ -7,9 +7,11 @@ Run it with:   python week1/hello.py
 """
 
 # TODO: print a greeting
-
+print("Greetings, Everyone")
 
 # TODO: create a variable with your name, and use it in a message
-
+name = ("Wisdom Amuzu")
 
 # TODO: try an f-string  ->  print(f"Hello, {name}!")
+my_message = ("Hello, Let's code")
+print(my_message + name)
